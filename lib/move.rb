a743932da93416457ba42c6647bd8f26c7e1c24c
William J Teskey <willjteskey@gmail.com>
2018-03-21 01:32:39 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,5 +6,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(player_input)
+  player_input.to_i - 1
+end
 
+def move(board, index, token = "X" )
+  board[index] = token
+end
 
